e15ccb1b26be3ff84a2d36c969d1a5af03ca8679
Jamayka Jones <jamayka.jones@gmail.com>
2016-06-27 22:12:06 -0400

    Added risk score
    Changed Survey Index View page

--- CentralNNApp/Controllers/SurveysController.cs
+++ CentralNNApp/Controllers/SurveysController.cs
@@ -53,140 +53,287 @@ namespace CentralNNApp.Controllers
             if (ModelState.IsValid)
             {
                 // ENTER RISK SCORE CODE HERE!!!
-                double risk_Score = 0f;​
+                double risk_Score = 0;
+
                 //QUESTION 1 - Ward
                 double ward = Convert.ToInt32(survey.Ward);
+                { 
+                if (ward == 1) {
+                    risk_Score += 53;
+                }
+                else if (ward == 2) {
+                    risk_Score += 17;
+                }
+                else if (ward == 3) {
+                    risk_Score += 18;
+                }
+                else if (ward == 4) {
+                    risk_Score += 16;
+                }
+                else if (ward == 5) {
+                    risk_Score += 37;
+                }
+                else if (ward == 6) {
+                    risk_Score += 15;
+                }
+                else if (ward == 7) {
+                    risk_Score += 19;
+                }
+                else if (ward == 8)
                 {
-                    if (ward == 1) {
-                        risk_Score += 53;
-                    }
-                    else if (ward == 2) {
-                        risk_Score += 17;
-                    }
-                    else if (ward == 3) {
-                        risk_Score += 18;
-                    }
-                    else if (ward == 4) {
-                        risk_Score += 16;
-                    }
-                    else if (ward == 5) {
-                        risk_Score += 37;
-                    }
-                    else if (ward == 6) {
-                        risk_Score += 15;
-                    }
-                    else if (ward == 7) {
-                        risk_Score += 19;
-                    }
-                    else if (ward == 8)
-                    {
-                        risk_Score += 15;
-                    }
-                    else if (ward == 9)
-                    {
-                        risk_Score += 33;
-                    }
-                    else if (ward == 10)
-                    {
-                        risk_Score += 32;
-                    }
-                    else if (ward == 11)
-                    {
-                        risk_Score += 20;
-                    }
-                    else if (ward == 12)
-                    {
-                        risk_Score += 21;
-                    }
-                    else if (ward == 13)
-                    {
-                        risk_Score += 16;
-                    }
-                    else if (ward == 14)
-                    {
-                        risk_Score += 21;
-                    }
-                    else if (ward == 15)
-                    {
-                        risk_Score += 38;
-                    }
-                    else if (ward == 16)
-                    {
-                        risk_Score += 31;
-                    }
-                    else if (ward == 17)
-                    {
-                        risk_Score += 10;
-                    }
-
-                    //QUESTION 2 - RACE
-                   ​
-                    int race = Convert.ToInt32(survey.Race);
-    ​
-                    if (race == 1)
-                    {
-                        risk_Score += 2;
-                    }
-                    else
-                    {
-                        risk_Score += 0;
-                    }
-​
-                    // QUESTION 3 - PREMATURE BIRTH
-                    int prem_birth = Convert.ToInt32(survey.Premature);
-​
-                    if (prem_birth == 1)
-                    {
-                        risk_Score += 5;
-                    }
-                    else
-                    {
-                        risk_Score += 0;
-                    }
-
-                    // QUESTION 4 - OBGYN CARE
-                    int obgyn_care = Convert.ToInt32(survey.OBGYN);
-​
-                    if (obgyn_care == 2)
-                    {
-                        risk_Score += 5;
-                    }
-                    else
-                    {
-                        risk_Score += 0;
-                    }
-
-                    // QUESTION 5 - AGE
-                    int age = Convert.ToInt32(survey.Age);
-​
-                    if (age == 1)
-                    {
-                        risk_Score += 4;
-                    }
-                    else if (age == 2)
-                    {
-                        risk_Score += 2;
-                    }
-                    else if (age == 4)
-                    {
-                        risk_Score += 3;
-                    }
-                    else
-                    {
-                        risk_Score += 0;
-                    }​
-                   ​
-                    //calculate risk score
-                    survey.RiskScore = risk_Score;
-
-                    db.Surveys.Add(survey);
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
+                    risk_Score += 15;
+                }
+                else if (ward == 9)
+                {
+                    risk_Score += 33;
+                }
+                else if (ward == 10)
+                {
+                    risk_Score += 32;
+                }
+                else if (ward == 11)
+                {
+                    risk_Score += 20;
+                }
+                else if (ward == 12)
+                {
+                    risk_Score += 21;
+                }
+                else if (ward == 13)
+                {
+                    risk_Score += 16;
+                }
+                else if (ward == 14)
+                {
+                    risk_Score += 21;
+                }
+                else if (ward == 15)
+                {
+                    risk_Score += 38;
+                }
+                else if (ward == 16)
+                {
+                    risk_Score += 31;
+                }
+                else if (ward == 17)
+                {
+                    risk_Score += 10;
                 }
 
+                //QUESTION 2 - RACE
+                int race = Convert.ToInt32(survey.Race);
+    
+                if (race == 1)
+                {
+                    risk_Score += 2;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+                    
+                // QUESTION 3 - PREMATURE BIRTH
+                int prem_birth = Convert.ToInt32(survey.Premature);
+
+                if (prem_birth == 1)
+                {
+                    risk_Score += 5;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                // QUESTION 4 - OBGYN CARE
+                int obgyn_care = Convert.ToInt32(survey.OBGYN);
+
+                if (obgyn_care == 2)
+                {
+                    risk_Score += 5;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                // QUESTION 5 - AGE
+                int age = Convert.ToInt32(survey.Age);
+                if (age == 1)
+                {
+                    risk_Score += 4;
+                }
+                else if (age == 2)
+                {
+                    risk_Score += 2;
+                }
+                else if (age == 4)
+                {
+                    risk_Score += 3;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                // QUESTION 6 - stress level
+                int stressLevel = Convert.ToInt32(survey.StressLevel);
+                if (stressLevel == 1)
+                {
+                    risk_Score += 2;
+                }
+                else if (stressLevel == 2)
+                {
+                    risk_Score += 1;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+                // QUESTION 7 - self smoke 
+                int smoker = Convert.ToInt32(survey.Smoker);
+                if (smoker == 1)
+                {
+                    risk_Score += 4;
+                }
+                else if (smoker == 2)
+                {
+                    risk_Score += 0;
+                }
+                else if (smoker == 3)
+                {
+                    risk_Score += 0;
+                }
+                //QUESTION 8 - family smoke
+                int familySmoke = Convert.ToInt32(survey.FamilySmoke);
+                if (familySmoke == 1)
+                {
+                    risk_Score += 2;
+                }
+                else if (familySmoke == 2)
+                {
+                    risk_Score += 0;
+                }
+
+                //QUESTION 9 - self alcohol
+                int alcohol = Convert.ToInt32(survey.Alcohol);
+                if (alcohol == 1)
+                {
+                    risk_Score += 4;
+                }
+                else if (alcohol == 2)
+                {
+                    risk_Score += 0;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                //QUESTION 10 - family alcohol
+                int familyalcohol = Convert.ToInt32(survey.FamilyAlcohol);
+                if (familyalcohol == 1)
+                {
+                    risk_Score += 2;
+                }
+                else if (familyalcohol == 2)
+                {
+                    risk_Score += 0;
+                }
+
+                //QUESTION 11 - slef drug use 
+                int drugUse = Convert.ToInt32(survey.Drug);
+                if (drugUse == 1)
+                {
+                    risk_Score += 4;
+                }
+                else if (drugUse == 2)
+                {
+                    risk_Score += 0;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                //QUESTION 12 - family drug use
+                int familyDrugs = Convert.ToInt32(survey.FamilyDrugs);
+                if (familyDrugs == 1)
+                {
+                    risk_Score += 2;
+                }
+                else if (familyDrugs == 2)
+                {
+                    risk_Score += 0;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                // QUESTION 13 - DIET
+                int diet = Convert.ToInt32(survey.DietBoolean);
+
+                if (diet == 2)
+                {
+                    risk_Score += 0.5;
+                }
+                else if (diet == 3)
+                {
+                    risk_Score += 1;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                // QUESTION 14 - CHRONIC ILLNESS
+                int illness = Convert.ToInt32(survey.ChronicIllness);
+                   
+                if (illness == 1)
+                {
+                    risk_Score += 3;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                // QUESTION 15 - INCOME
+                int income = Convert.ToInt32(survey.Income);
+
+                if (income == 1)
+                {
+                    risk_Score += 4;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+
+                // QUESTION 16 - EDUCATION
+                int education = Convert.ToInt32(survey.Education);
+                if (education == 1)
+                {
+                    risk_Score += 2;
+                }
+                else if (education == 2)
+                {
+                    risk_Score += 1;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }    
+                //calculate risk score
+                survey.RiskScore = risk_Score;
+
+                db.Surveys.Add(survey);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
                 ViewBag.MotherID = new SelectList(db.Mothers, "ID", "FirstName", survey.MotherID);
                 return View(survey);
-            }
         }
 
         // GET: Surveys/Edit/5
